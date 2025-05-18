0b4294c3fd4c418956fd5f2773c38adcb108925f
Syahrul-0036 <124240005@student.upnyk.ac.id>
2025-05-18 10:02:12 +0700

    Delete herbal/finalproject1 directory

--- herbal/finalproject1/herbacare/proses/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start(); 
-    session_destroy(); 
-    echo "<script>
-            alert('Logout Berhasil, byee byeeee!!!1!!');
-            window.location.href = '../pages/login.php';
-            </script>";
-?>
